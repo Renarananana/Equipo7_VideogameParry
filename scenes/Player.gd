f72c6d26bba675fe7a72970c74ea67757b3040b1
Renaranananna <Renato.andaur@gmail.com>
2022-04-03 20:37:41 -0400

    Animations
    animaciones para caminar a todos los lados

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -3,6 +3,9 @@ extends KinematicBody2D
 var SPEED = 200
 var ACCELERATION = 100
 var velocity = Vector2()
+onready var animTree = $AnimationTree
+onready var animPlayer = $AnimationPlayer
+onready var playback = animTree.get("parameters/playback")
 
 
 func _physics_process(delta):
@@ -13,5 +16,24 @@ func _physics_process(delta):
 	var move_input_y = Input.get_axis("up", "down")
 	
 	velocity.x = move_toward(velocity.x, move_input_x * SPEED, ACCELERATION)
-	velocity.y = move_toward(velocity.y, move_input_y * SPEED, ACCELERATION) 
+	velocity.y = move_toward(velocity.y, move_input_y * SPEED, ACCELERATION)
+	
+	if velocity.x > 10 and abs(velocity.y) <10:
+		playback.travel("walk_right")
+	elif velocity.x > 10 and velocity.y >10:
+		playback.travel("walk_frontright")
+	elif velocity.x < -10 and abs(velocity.y) <10:
+		playback.travel("walk_left")
+	elif velocity.x > 10 and velocity.y < -10:
+		playback.travel("walk_backright")
+	elif velocity.y < -10 and abs(velocity.x) <10:
+		playback.travel("walk_back")
+	elif velocity.x < -10 and velocity.y < -10:
+		playback.travel("walk_backleft")
+	elif velocity.y > 10 and abs(velocity.x) <10:
+		playback.travel("walk_front")
+	elif velocity.x < -10 and velocity.y > 10:
+		playback.travel("walk_frontleft")
+	else:
+		playback.travel("Idle") 
 	
