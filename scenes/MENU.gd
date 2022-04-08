1d4b7941a460260986641e98dcd33c2d57ef6fb9
shamivejar12 <102923357+shamivejar12@users.noreply.github.com>
2022-04-08 15:40:40 -0400

    menu

---
+++ scenes/MENU.gd
@@ -0,0 +1,19 @@
+extends MarginContainer
+
+onready var play=$Panel/VBoxContainer/play
+onready var exit=$Panel/VBoxContainer/exit
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	play.connect("pressed",self,"_on_play_pressed")
+	exit.connect("pressed",self,"_on_exit_pressed")
+
+func _on_play_pressed():
+	get_tree().change_scene("res://scenes/Player.tscn")
+	
+
+func _on_exit_pressed():  #esta func no recibe argumento 
+	get_tree().quit()    #salida 
+	
